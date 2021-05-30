a84cbdc5dcb40dd35d6c20e784fc9fdb94aee11f
Vladimír Aubrecht <vladimir.aubrecht@me.com>
2021-05-30 20:21:34 +0200

    Empty app

---
+++ src/GeoBypasser/GeoBypasser/GeoBypasserApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoBypasserApp.swift
+//  GeoBypasser
+//
+//  Created by Vladimir Aubrecht on 30.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct GeoBypasserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
